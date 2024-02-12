cc918a163e2b2af594732ed91219fa16832c3b90
Ivan Kuznetsov <mrjohnny@mail.ru>
2024-02-12 17:42:06 +0300

    add: new functions

--- mymath.go
+++ mymath.go
@@ -2,7 +2,6 @@ package mymath
 
 import "math"
 
-// Sqrt - функция для вычисления квадратного корня из числа
 func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
 }
@@ -34,3 +33,15 @@ func ABS(x float64) float64 {
 func Yn(n int, x float64) float64 {
 	return math.Yn(n, x)
 }
+
+func Sin(x float64) float64 { return math.Sin(x) }
+
+func Cos(x float64) float64 { return math.Cos(x) }
+
+func Acos(x float64) float64 { return math.Acos(x) }
+
+func Acosh(x float64) float64 { return math.Acosh(x) }
+
+func Asin(x float64) float64 { return math.Asin(x) }
+
+func Asinh(x float64) float64 { return math.Asinh(x) }
